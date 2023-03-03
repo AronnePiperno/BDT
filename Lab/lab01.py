83e65e372fe10106af7c3f55c988437fa2bb4410
Ale <deferrari.alessandro@gmail.com>
2023-03-03 10:18:26 +0100

    lab01

--- Lab/lab01.py
+++ Lab/lab01.py
@@ -1,3 +1,6 @@
+import random
+
+
 def warmup(a,b):
     a = int(input('insert an integer: '))
     b = int(input('insert an integer: '))
@@ -7,4 +10,52 @@ def warmup(a,b):
         return a*b
 
 
-print(warmup(a,b))
+def fibonacci():
+    n = int(input('Insert a number: '))
+    liston = [0,1]
+    for i in range(2, 100):
+        liston.append(liston[i-1] + liston[i-2])
+    
+    return n in liston
+
+def pantry():
+    recipe = {'eggs' : 2, 'flour': 200, 'milk': 100}
+    pantry = {'eggs': 12, 'flour': 1000, 'milk': 250, 'apple': 2, 'sugar': 850}
+    liston = []
+    for k in recipe:
+        if k in pantry:
+            if pantry[k] > recipe[k]:
+                liston.append(pantry[k] // recipe[k])
+        else:
+            return 0
+        
+    return min(liston)
+
+
+def ex5():
+    n = int(input('Insert a number: '))
+    summy = 0
+    for i in range(1,n+1):
+        summy += i/(i+1)
+    return summy
+
+
+def ex6():
+    
+    return_value = random.randint(0,100)
+    while return_value % 2 == 0:
+        return_value = random.randint(0,100)
+    
+    return return_value
+
+
+def ex7(liston):
+    for n in range(0, len(liston),2):
+        print(liston[n])
+
+ex7([3,2,4,5])
+#print(pantry())
+
+
+
+#print(fibonacci())
